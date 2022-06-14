981d63e9ee8c8af374ef70314d2029692a860924
Tom Feist-Hassan <tom.feist-hassan@camresp.com>
2022-06-14 10:36:49 +0100

    start re-integrating json output

--- src/poetry_plugin_export/exporter.py
+++ src/poetry_plugin_export/exporter.py
@@ -27,9 +27,14 @@ class Exporter:
     """
 
     FORMAT_REQUIREMENTS_TXT = "requirements.txt"
+    FORMAT_GROUPS_JSON = 'grouped-requirements.json'
+
     ALLOWED_HASH_ALGORITHMS = ("sha256", "sha384", "sha512")
 
-    EXPORT_METHODS = {FORMAT_REQUIREMENTS_TXT: "_export_requirements_txt"}
+    EXPORT_METHODS = {
+        FORMAT_REQUIREMENTS_TXT: "_export_requirements_txt",
+        FORMAT_GROUPS_JSON: "_export_grouped_requirements_json",
+    }
 
     def __init__(self, poetry: Poetry) -> None:
         self._poetry = poetry
@@ -73,6 +78,111 @@ class Exporter:
             raise ValueError(f"Invalid export format: {fmt}")
 
         getattr(self, self.EXPORT_METHODS[fmt])(cwd, output)
+
+    def _export_grouped_requirements_json(self, cwd: Path, output: IO | str) -> None:
+
+        import json
+
+        dependency_items = []
+        root, packages = self._build_packages_for_export()
+
+        name_to_pkg = { p.name: p for p in packages }
+
+        def walk_deps(dep):
+            pkg = name_to_pkg[dep.name]
+            yield pkg
+            for subdep in pkg.all_requires:
+                yield from walk_deps(subdep)
+
+        pkgs_for_group = {}
+        groups_for_pkg ={}
+        for dep in root.all_requires:
+            pkgs_in_groups = list(walk_deps(dep))
+            for g in dep.groups:
+                pkgs_for_group.setdefault(g, set()).update(p.name for p in pkgs_in_groups)
+
+            for p in pkgs_in_groups:
+                groups_for_pkg.setdefault(p.name, set()).update(dep.groups)
+
+
+        dependency_packages = list(self._poetry.locker.get_project_dependency_packages(
+            project_requires=root.all_requires,
+            dev=True,
+            extras=self._extras,
+        ))
+
+        for dependency_package in dependency_packages:
+
+            entry = {}
+            req_txt_line = ''
+            dependency = dependency_package.dependency
+            package = dependency_package.package
+
+            if package not in packages:
+                continue
+
+
+            if package.develop:
+                entry['develop'] = True
+
+            requirement = dependency.to_pep_508(with_extras=False)
+            is_direct_local_reference = (
+                dependency.is_file() or dependency.is_directory()
+            )
+            is_direct_remote_reference = dependency.is_vcs() or dependency.is_url()
+
+            if is_direct_remote_reference:
+                req_txt_line = requirement
+            elif is_direct_local_reference:
+                dependency_uri = path_to_url(package.source_url)
+                req_txt_line = f"{package.name} @ {dependency_uri}"
+                entry['name'] = package.name
+                entry['url'] = dependency_uri
+            else:
+                entry['name'] = package.name
+                entry['version'] = str(package.version)
+                req_txt_line = f"{package.name}=={package.version}"
+
+            if not is_direct_remote_reference:
+                if ";" in requirement:
+                    markers = requirement.split(";", 1)[1].strip()
+                    if markers and self._with_markers:
+                        entry['markers'] = markers
+                        req_txt_line += f"; {markers}"
+
+            if (
+                not is_direct_remote_reference
+                and not is_direct_local_reference
+                and package.source_url
+            ):
+                entry['source_url'] = package.source_url
+
+            if package.files and self._with_hashes:
+                hashes = []
+                for f in package.files:
+                    h = f["hash"]
+                    algorithm = "sha256"
+                    if ":" in h:
+                        algorithm, h = h.split(":")
+
+                        if algorithm not in self.ALLOWED_HASH_ALGORITHMS:
+                            continue
+
+                    hashes.append(f"{algorithm}:{h}")
+
+                if hashes:
+                    entry['hashes'] = list(hashes)
+                    req_txt_line += " \\\n"
+                    for i, h in enumerate(hashes):
+                        req_txt_line += "    --hash={}{}".format(
+                            h, " \\\n" if i < len(hashes) - 1 else ""
+                        )
+
+            entry['groups'] = list(groups_for_pkg[package.name])
+            entry['req_entry'] = req_txt_line
+            dependency_items.append(entry)
+
+        self._output(json.dumps(dependency_items, indent=2), cwd, output)
 
     def _export_requirements_txt(self, cwd: Path, output: IO | str) -> None:
         from poetry.core.packages.utils.utils import path_to_url
